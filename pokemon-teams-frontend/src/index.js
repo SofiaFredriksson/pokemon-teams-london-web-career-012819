0cc49b5ed6e3a6c84f8242a4339c9463cd8c7e51
SofiaFredriksson <sofiapkf@gmail.com>
2019-07-26 13:22:44 +0100

    pokemons and trainer cards

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -2,11 +2,51 @@ const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
 
+const mainEl = document.querySelector('main')
+
 const getTrainers = () => {
-    fetch(TRAINERS_URL)
+    return fetch(TRAINERS_URL)
     .then(resp => resp.json())
 }
 
-const showTrainer = ()  => {
-    
+const showTrainer = (trainer)  => {
+    const cardEl = document.createElement('div')
+    cardEl.classList.add("card")
+    cardEl.dataset.id = trainer.id
+
+
+
+    cardEl.innerHTML = 
+    `
+        <p>${trainer.name}</p>
+        <button data-trainer-id=${trainer.id}>Add Pokemon</button>
+        <ul>
+            ${trainer.pokemons.map(showPokemon).join("")}
+        </ul>
+
+    `
+
+    mainEl.appendChild(cardEl)
 }
+
+const showPokemon = (pokemon) => {
+    
+    return `
+        <li>
+        ${pokemon.nickname} (${pokemon.species})
+        <button class="release" data-pokemon-id=${pokemon.id}>Release</button>
+        </li>
+    `
+
+}
+
+const showAllTrainers = (trainers) => {
+    trainers.forEach(showTrainer)
+}
+
+const initialize = () => {
+    getTrainers()
+    .then(showAllTrainers)
+}
+
+initialize()
