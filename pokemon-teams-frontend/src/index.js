4bcb69f335138912cf62b817c3e297638c4d86cb
SofiaFredriksson <sofiapkf@gmail.com>
2019-07-26 11:13:54 +0100

    first commit

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,12 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+const getTrainers = () => {
+    fetch(TRAINERS_URL)
+    .then(resp => resp.json())
+}
+
+const showTrainer = ()  => {
+    
+}
