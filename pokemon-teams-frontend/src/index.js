e55057ccaf558ad66d2569b5c2ff8a405b5ab194
SofiaFredriksson <sofiapkf@gmail.com>
2019-07-31 14:58:04 +0100

    done

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -4,49 +4,95 @@ const POKEMONS_URL = `${BASE_URL}/pokemons`
 
 const mainEl = document.querySelector('main')
 
-const getTrainers = () => {
-    return fetch(TRAINERS_URL)
-    .then(resp => resp.json())
-}
+// client side
 
 const showTrainer = (trainer)  => {
     const cardEl = document.createElement('div')
     cardEl.classList.add("card")
     cardEl.dataset.id = trainer.id
 
-
-
     cardEl.innerHTML = 
     `
         <p>${trainer.name}</p>
-        <button data-trainer-id=${trainer.id}>Add Pokemon</button>
+        <button class='create' data-trainer-id=${trainer.id}>Add Pokemon</button>
         <ul>
             ${trainer.pokemons.map(showPokemon).join("")}
         </ul>
 
     `
 
+    const createBtn = cardEl.querySelector('.create')
+    const ulEl = cardEl.querySelector('ul')
+    createBtn.addEventListener('click', () => {
+        addPokemon(trainer.id)
+        .then(pokemon => {
+            if (pokemon.error) return 
+           ulEl.innerHTML += `
+           <li id='pokemon-${pokemon.id}'>
+           ${pokemon.nickname} (${pokemon.species})
+           <button class="release" data-pokemon-id=${pokemon.id}>Release</button>
+           </li>
+       `
+        })
+    }) 
     mainEl.appendChild(cardEl)
 }
 
-const showPokemon = (pokemon) => {
-    
+const showPokemon = (pokemon) => {  
     return `
-        <li>
+        <li id='pokemon-${pokemon.id}'>
         ${pokemon.nickname} (${pokemon.species})
         <button class="release" data-pokemon-id=${pokemon.id}>Release</button>
         </li>
     `
-
 }
 
 const showAllTrainers = (trainers) => {
     trainers.forEach(showTrainer)
 }
 
+
+//listeners 
+const addPokemonRemoverListener = () => {
+    document.addEventListener('click', event => {
+        if (event.target.classList.contains('release')){
+            const id = event.target.dataset.pokemonId
+            const liEl = document.querySelector(`#pokemon-${id}`)
+            deletePokemon(id)
+            .then(() => {
+                liEl.remove()
+              })
+              .catch(() => alert("Woops! Try again!"))
+        }
+    })
+}
+
+
+
+//server side
+const deletePokemon = (pokemonId) => {
+	return fetch(POKEMONS_URL + `/${pokemonId}`, {
+		method: 'DELETE'
+	}).then(resp => resp.json())
+}
+
+const getTrainers = () => {
+    return fetch(TRAINERS_URL)
+    .then(resp => resp.json())
+}
+
+const addPokemon = (trainerId) => {
+   return fetch(POKEMONS_URL, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({trainer_id: trainerId})
+    }).then(resp => resp.json())
+}
+
 const initialize = () => {
     getTrainers()
     .then(showAllTrainers)
+    addPokemonRemoverListener()
 }
 
 initialize()
